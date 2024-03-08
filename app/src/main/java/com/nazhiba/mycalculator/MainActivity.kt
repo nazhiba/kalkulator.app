e95af117629757954be4c29a201d5202aa9b3584
Nadzib <nadzib22@gmail.com>
2024-03-08 08:03:36 +0700

    add button

--- app/src/main/java/com/nazhiba/mycalculator/MainActivity.kt
+++ app/src/main/java/com/nazhiba/mycalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nazhiba.mycalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+    }
+    fun onDigit(view: View){
+        Toast.makeText(this,"Button clicked", Toast.LENGTH_LONG).show()
     }
 }
